9adfd484a03f5ffb9aaaf0d91c48e82d4f90dc43
mafuhua <862698384@qq.com>
2016-01-16 16:05:27 +0800

    测试提交

---
+++ app/src/main/java/com/example/hellofuhua/mygoolgleplay/MyTest.java
@@ -0,0 +1,14 @@
+package com.example.hellofuhua.mygoolgleplay;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hellofuhua on 2016/1/16.
+ */
+public class MyTest extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
